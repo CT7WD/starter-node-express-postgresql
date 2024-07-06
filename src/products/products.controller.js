72e031399d685b4430d4f6ed70fc1b80762ec28b
CT7WD <63139550+CT7WD@users.noreply.github.com>
2024-07-06 06:24:51 -0400

    Added productExists validation, and added notes.  Also added 'read' function.

--- src/products/products.controller.js
+++ src/products/products.controller.js
@@ -8,6 +8,41 @@ function list(req, res, next) {
     .catch(next);
 }
 
+// Middleware to validate whether product exists or not based on ID.
+// Create function named "productExists" that has access to
+// request, response, & next middleware function in apps 
+// request-response cycle.
+function productExists(req, res, next) {
+  productsService
+  // ^ object that 'read' is being called on.
+  // 'read' is a method of this object to retrieves a product
+  // based on ID.
+  .read(req.params.productId)
+  // 'req.params.productId access the 'productId parameter from
+  // URL of incoming request.
+  .then((product) => {
+    if (product) {
+      res.locals.product = product;
+      // ^ if 'product' exist, it is stored in
+      // 'res.locals.product local variable to be readily accessed in the rest of middleware pipeline.
+      return next();
+      // ^ if 'product' does not exist, 'next' is called with an error.
+    }
+    next({ status: 404, message: `Product cannot be found.` });
+  })
+  .catch(next);
+}
+
+// READ
+function read(req, res) {
+  const { product: data } = res.locals;
+  res.json({ data });
+}
+
 module.exports = {
+  read: [
+    productExists,
+    read
+  ],
   list,
 }
--- src/products/products.service.js
+++ src/products/products.service.js
@@ -1,9 +1,19 @@
 const knex = require("../db/connection");
 
+// GET a list of all products
 function list() {
   return knex("products").select("*");
 }
 
+// GET a specific product, based on its ID
+// SELECT all cols from PRODUCTS table where product_id col matches passed in arg.
+// first() returns first row in table.
+function read(productId) {
+  return knex("product").select("*").where({ product_id: productId }).first()
+}
+
+
 module.exports = {
   list,
+  read
 };
